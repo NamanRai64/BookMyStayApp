a73513c60eaae42e73f3260305632cc76a67d4a5
NamanRai64 <namanrai1712@gmail.com>
2026-03-16 23:26:00 +0530

    Use Case 2 room structuring

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Room {
+public abstract class Room {
+    protected String roomType;
+    protected int numberOfBeds;
+    protected double size;
+    protected double price;
+
+    public Room(String roomType, int numberOfBeds, double size, double price) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public abstract void displayRoomDetails();
+
+    // Getters
+    public String getRoomType() { return roomType; }
+    public int getNumberOfBeds() { return numberOfBeds; }
+    public double getSize() { return size; }
+    public double getPrice() { return price; }
 }
